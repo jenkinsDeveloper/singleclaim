1e9e7b5c93e01488c32b8a831934e0a3794675de
Thomas Orgeldinger <thomas.orgeldinger@icongmbh.de>
2014-03-17 15:05:29 +0100

    initialize project

---
+++ de.icongmbh.singleclaim/src/main/java/de/icongmbh/singleclaim/TestClaim.java
@@ -0,0 +1,17 @@
+package de.icongmbh.singleclaim;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClaim {
+
+  @Test
+  public void testFail() {
+    Assert.fail();
+  }
+
+  @Test
+  public void testNotFail() {
+
+  }
+}

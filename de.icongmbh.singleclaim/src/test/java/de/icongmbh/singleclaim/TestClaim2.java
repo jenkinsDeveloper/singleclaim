d4a7feed4aa1acf5a9625f4a390e96e8d2d8d4ac
Thomas Orgeldinger <thomas.orgeldinger@icongmbh.de>
2014-03-18 18:56:32 +0100

    added test 2

---
+++ de.icongmbh.singleclaim/src/test/java/de/icongmbh/singleclaim/TestClaim2.java
@@ -0,0 +1,37 @@
+package de.icongmbh.singleclaim;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClaim2 {
+
+  @Test
+  public void testFail() {
+    Assert.fail();
+  }
+  
+  @Test
+  public void testFail2() {
+    Assert.fail();
+  }
+  
+  @Test
+  public void testFail3() {
+    Assert.fail();
+  }
+
+  @Test
+  public void testNotFail() {
+
+  }
+  
+  @Test
+  public void testNotFail2() {
+
+  }
+  
+  @Test
+  public void testNotFail3() {
+
+  }
+}
